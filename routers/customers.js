f76cfcb07f65c65bcc12973481134bc25c6b6cbc
Clone <40709168+Cloneee@users.noreply.github.com>
2022-05-30 22:03:30 +0700

    Add customer order

--- routers/customers.js
+++ routers/customers.js
@@ -5,196 +5,191 @@ const bcrypt = require("bcrypt");
 var jwt = require("jsonwebtoken");
 
 router
-	.route("/")
-	.get(function (req, res) {
-		Customer.find(function (err, foundCustomerList) {
-			if (!err) {
-				if (foundCustomerList.length === 0) {
-					return res.status(200).json({
-						message: "Empty Customer",
-					});
-				}
-				return res.send(foundCustomerList);
-			} else {
-				return res.status(500).json({
-					success: false,
-					error: err,
-				});
-			}
-		}).select("-password");
-	})
-	.post(function (req, res) {
-		let salt = parseInt(process.env.SALT_ROUND);
-		let hashPassWord = bcrypt.hashSync(req.body.password, salt);
-		let newCustomer = new Customer({
-			name: req.body.name,
-			email: req.body.email,
-			password: hashPassWord,
-			phone: req.body.phone,
-			address: req.body.address,
-		});
-		newCustomer.save(function (err) {
-			if (!err) {
-				return res
-					.status(200)
-					.send("Successfully added a new Customer.");
-			} else {
-				return res.status(400).json({
-					success: false,
-					error: err.message,
-					status: "A Customer cannot be created",
-				});
-			}
-		});
-	});
+  .route("/")
+  .get(function (req, res) {
+    Customer.find(function (err, foundCustomerList) {
+      if (!err) {
+        if (foundCustomerList.length === 0) {
+          return res.status(200).json({
+            message: "Empty Customer",
+          });
+        }
+        return res.send(foundCustomerList);
+      } else {
+        return res.status(500).json({
+          success: false,
+          error: err,
+        });
+      }
+    }).select("-password");
+  })
+  .post(function (req, res) {
+    let salt = parseInt(process.env.SALT_ROUND);
+    let hashPassWord = bcrypt.hashSync(req.body.password, salt);
+    let newCustomer = new Customer({
+      name: req.body.name,
+      email: req.body.email,
+      password: hashPassWord,
+      phone: req.body.phone,
+      address: req.body.address,
+    });
+    newCustomer.save(function (err) {
+      if (!err) {
+        return res.status(200).send("Successfully added a new Customer.");
+      } else {
+        return res.status(400).json({
+          success: false,
+          error: err.message,
+          status: "A Customer cannot be created",
+        });
+      }
+    });
+  });
 
-router.post("/search", async (req,res)=>{
-	try {
-		let result = await Customer.find({$text: {$search: req.body.text}}).select("-__v -password")
-		res.json(result)
-	} catch (error) {
-		console.log(error)
-		res.status(400).json({err: error})
-	}
-})
+router.post("/search", async (req, res) => {
+  try {
+    let result = await Customer.find({
+      $text: { $search: req.body.text },
+    }).select("-__v -password");
+    res.json(result);
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ err: error });
+  }
+});
 
 router
-	.route("/:customerId")
-	.get(function (req, res) {
-		Customer.findOne(
-			{ _id: req.params.customerId },
-			function (err, foundCustomer) {
-				if (foundCustomer) {
-					return res.send(foundCustomer);
-				}
-				return res.status(404).json({
-					success: false,
-					message: "A Customer with the given ID was not found",
-				});
-			}
-		).select("-password");
-	})
-	.put(async function (req, res) {
-		const customerfound = await Customer.findById(req.params.customerId);
-		if (!customerfound) {
-			return res.status(404).json({
-				success: false,
-				message: "Customer ID does not exist!",
-			});
-		}
-		Customer.findOneAndUpdate(
-			{ _id: req.params.customerId },
-			{
-				name: req.body.name,
-				email: req.body.email,
-				password: req.body.password
-					? bcrypt.hashSync(
-							req.body.password,
-							parseInt(process.env.SALT_ROUND)
-					  )
-					: customerfound.password,
-				phone: req.body.phone,
-				address: req.body.address,
-			},
-			{ new: true },
-			function (err) {
-				if (!err) {
-					res.status(200).send(
-						"Successfully updated the selected Customer."
-					);
-				} else {
-					res.status(404).json({
-						success: false,
-						error: err.message,
-						status: "A Customer cannot updated",
-					});
-				}
-			}
-		);
-	})
-	.delete(function (req, res) {
-		Customer.findOneAndDelete(
-			{ _id: req.params.customerId },
-			function (err, foundEmployee) {
-				if (!err) {
-					if (!foundEmployee) {
-						return res.status(404).json({
-							message: "Customer does not exist.",
-						});
-					}
-					return res.status(200).json({
-						success: true,
-						message: "Successfully deleted a select Customer",
-					});
-				} else {
-					return res.status(404).json({
-						sucess: false,
-						error: err.message,
-						message: "Invalid Customer ID",
-						status: "A Customer cannot deleted.",
-					});
-				}
-			}
-		);
-	});
+  .route("/:customerId")
+  .get(function (req, res) {
+    Customer.findOne(
+      { _id: req.params.customerId },
+      function (err, foundCustomer) {
+        if (foundCustomer) {
+          return res.send(foundCustomer);
+        }
+        return res.status(404).json({
+          success: false,
+          message: "A Customer with the given ID was not found",
+        });
+      }
+    ).select("-password");
+  })
+  .put(async function (req, res) {
+    const customerfound = await Customer.findById(req.params.customerId);
+    if (!customerfound) {
+      return res.status(404).json({
+        success: false,
+        message: "Customer ID does not exist!",
+      });
+    }
+    Customer.findOneAndUpdate(
+      { _id: req.params.customerId },
+      {
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+          ? bcrypt.hashSync(req.body.password, parseInt(process.env.SALT_ROUND))
+          : customerfound.password,
+        phone: req.body.phone,
+        address: req.body.address,
+      },
+      { new: true },
+      function (err) {
+        if (!err) {
+          res.status(200).send("Successfully updated the selected Customer.");
+        } else {
+          res.status(404).json({
+            success: false,
+            error: err.message,
+            status: "A Customer cannot updated",
+          });
+        }
+      }
+    );
+  })
+  .delete(function (req, res) {
+    Customer.findOneAndDelete(
+      { _id: req.params.customerId },
+      function (err, foundEmployee) {
+        if (!err) {
+          if (!foundEmployee) {
+            return res.status(404).json({
+              message: "Customer does not exist.",
+            });
+          }
+          return res.status(200).json({
+            success: true,
+            message: "Successfully deleted a select Customer",
+          });
+        } else {
+          return res.status(404).json({
+            sucess: false,
+            error: err.message,
+            message: "Invalid Customer ID",
+            status: "A Customer cannot deleted.",
+          });
+        }
+      }
+    );
+  });
 
 router.post("/login", function (req, res) {
-	Customer.findOne({ email: req.body.email }, function (err, foundCustomer) {
-		if (foundCustomer) {
-			if (bcrypt.compareSync(req.body.password, foundCustomer.password)) {
-				const secret = process.env.SECRET;
-				const token = jwt.sign(
-					{
-						customerId: foundCustomer._id,
-						isCustomer: true,
-					},
-					secret,
-					{
-						expiresIn: "1d",
-					}
-				);
-				return res.status(200).json({
-					email_customer: foundCustomer.email,
-					token: token,
-				});
-			} else {
-				return res.status(400).json({ message: "Password is wrong!" });
-			}
-		}
-		return res.status(400).json({ message: "Email user not found" });
-	});
+  Customer.findOne({ email: req.body.email }, function (err, foundCustomer) {
+    if (foundCustomer) {
+      if (bcrypt.compareSync(req.body.password, foundCustomer.password)) {
+        const secret = process.env.SECRET;
+        const token = jwt.sign(
+          {
+            customerId: foundCustomer._id,
+            isCustomer: true,
+          },
+          secret,
+          {
+            expiresIn: "1d",
+          }
+        );
+        return res.status(200).json({
+          email_customer: foundCustomer.email,
+          token: token,
+        });
+      } else {
+        return res.status(400).json({ message: "Password is wrong!" });
+      }
+    }
+    return res.status(400).json({ message: "Email user not found" });
+  });
 });
 
 router.post("/register", function (req, res) {
-	let salt = parseInt(process.env.SALT_ROUND);
-	let hashPassWord = bcrypt.hashSync(req.body.password, salt);
-	let newCustomer = new Customer({
-		name: req.body.name,
-		email: req.body.email,
-		password: hashPassWord,
-		phone: req.body.phone,
-		address: req.body.address,
-	});
-	newCustomer.save(function (err) {
-		if (!err) {
-			return res.status(200).send("Successful registration.");
-		} else {
-			return res.status(400).json({
-				success: false,
-				error: err.message,
-				status: "Registration failed.",
-			});
-		}
-	});
+  let salt = parseInt(process.env.SALT_ROUND);
+  let hashPassWord = bcrypt.hashSync(req.body.password, salt);
+  let newCustomer = new Customer({
+    name: req.body.name,
+    email: req.body.email,
+    password: hashPassWord,
+    phone: req.body.phone,
+    address: req.body.address,
+  });
+  newCustomer.save(function (err) {
+    if (!err) {
+      return res.status(200).send("Successful registration.");
+    } else {
+      return res.status(400).json({
+        success: false,
+        error: err.message,
+        status: "Registration failed.",
+      });
+    }
+  });
 });
 router.get("/get/count", function (req, res) {
-	Customer.countDocuments(function (err, count) {
-		if (!err) {
-			res.status(200).json({ customerCount: count });
-		} else {
-			res.send(err);
-		}
-	});
+  Customer.countDocuments(function (err, count) {
+    if (!err) {
+      res.status(200).json({ customerCount: count });
+    } else {
+      res.send(err);
+    }
+  });
 });
 
 module.exports = router;
--- routers/orders.js
+++ routers/orders.js
@@ -7,279 +7,374 @@ const Product = require("../models/product");
 const Customer = require("../models/customer");
 
 router
-	.route("/")
-	.get(function (req, res) {
-		let page = req.query.page ? Number(req.query.page) : 1,
-			limit = req.query.limit ? Number(req.query.limit) : 20
-		page < 1 || isNaN(page) ? page = 1 : null
-		limit < 1 || isNaN(limit) ? limit = 20 : null
+  .route("/")
+  .get(function (req, res) {
+    let page = req.query.page ? Number(req.query.page) : 1,
+      limit = req.query.limit ? Number(req.query.limit) : 20;
+    page < 1 || isNaN(page) ? (page = 1) : null;
+    limit < 1 || isNaN(limit) ? (limit = 20) : null;
 
-		Promise.all([
-			Order.find()
-				.sort({ role: 1 })
-				.select({ __v: false, password: false })
-				.limit(limit)
-				.skip((page - 1) * limit)
-				.populate("customer", "name")
-				.populate({
-					path: "orderItems",
-					populate: {
-						path: "product",
-						populate: {
-							path: "category",
-							select: "name"
-						},
-						select: "_id name price"
-					},
-					select: "-__v"
-				})
-				.populate("employee", "name role")
-				.sort({ dateOrdered: -1 })
-				.select("-__v"),
-			Order.count()
-		])
-			.then(([data, total]) => res.json({
-				currentPage: page,
-				limit: limit,
-				totalPage: total % limit != 0 ? parseInt(total / limit + 1) : total / limit,
-				data: data
-			}))
-			.catch(err => res.status(500).json({
-				isSuccess: false,
-				error: err,
-			}))
-	})
-	.post(async function (req, res) {
-		const orderItemsIds = Promise.all(
-			req.body.orderItems.map(async (orderitem) => {
-				const foundProduct = Product.findById(orderitem.product);
-				if (foundProduct) {
-					let newOrderItem = new OrderItem({
-						quantity: orderitem.quantity,
-						product: orderitem.product,
-					});
-					newOrderItem = await newOrderItem.save();
-					return newOrderItem._id;
-				}
-			})
-		).catch((err) => {
-			return -1;
-		});
-		const orderItemsIdsResolved = await orderItemsIds;
-		if (orderItemsIdsResolved === -1) {
-			return res.status(404).json({
-				error: "Invalid ID Product",
-				message: "Cannot create orders",
-			});
-		}
-		const totalPrices = await Promise.all(
-			orderItemsIdsResolved.map(async (orderItemId) => {
-				const orderItem = await (
-					await OrderItem.findById(orderItemId)
-				).populate("product", "price");
-				const totalPrice = orderItem.product.price * orderItem.quantity;
-				return totalPrice;
-			})
-		);
-		const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
-		let newOrder = new Order({
-			orderItems: orderItemsIdsResolved,
-			shippingAddress: req.body.shippingAddress,
-			address: req.body.address,
-			status: req.body.status,
-			totalPrice: totalPrice,
-			customer: req.body.customer,
-			employee: req.locals.payload.employeeId,
-		});
-		newOrder.save()
-			.then(() => {
-				Order.findById(newOrder._id)
-					.select({ __v: false, password: false })
-					.populate("customer", "name")
-					.populate({
-						path: "orderItems",
-						populate: {
-							path: "product",
-							populate: {
-								path: "category",
-								select: "name"
-							},
-							select: "_id name price"
-						},
-						select: "-__v"
-					})
-					.populate("employee", "name role")
-					.select("-__v")
-					.then(doc => res.json(doc))
-			})
-			.catch(e => {
-				res.status(400).json({
-					success: false,
-					error: e.message,
-					status: "The Order cannot be created",
-				});
-			})
-	});
+    Promise.all([
+      Order.find()
+        .sort({ role: 1 })
+        .select({ __v: false, password: false })
+        .limit(limit)
+        .skip((page - 1) * limit)
+        .populate("customer", "name")
+        .populate({
+          path: "orderItems",
+          populate: {
+            path: "product",
+            populate: {
+              path: "category",
+              select: "name",
+            },
+            select: "_id name price",
+          },
+          select: "-__v",
+        })
+        .populate("employee", "name role")
+        .sort({ dateOrdered: -1 })
+        .select("-__v"),
+      Order.count(),
+    ])
+      .then(([data, total]) =>
+        res.json({
+          currentPage: page,
+          limit: limit,
+          totalPage:
+            total % limit != 0 ? parseInt(total / limit + 1) : total / limit,
+          data: data,
+        })
+      )
+      .catch((err) =>
+        res.status(500).json({
+          isSuccess: false,
+          error: err,
+        })
+      );
+  })
+  .post(async function (req, res) {
+    const orderItemsIds = Promise.all(
+      req.body.orderItems.map(async (orderitem) => {
+        const foundProduct = Product.findById(orderitem.product);
+        if (foundProduct) {
+          let newOrderItem = new OrderItem({
+            quantity: orderitem.quantity,
+            product: orderitem.product,
+          });
+          newOrderItem = await newOrderItem.save();
+          return newOrderItem._id;
+        }
+      })
+    ).catch((err) => {
+      return -1;
+    });
+    const orderItemsIdsResolved = await orderItemsIds;
+    if (orderItemsIdsResolved === -1) {
+      return res.status(404).json({
+        error: "Invalid ID Product",
+        message: "Cannot create orders",
+      });
+    }
+    const totalPrices = await Promise.all(
+      orderItemsIdsResolved.map(async (orderItemId) => {
+        const orderItem = await (
+          await OrderItem.findById(orderItemId)
+        ).populate("product", "price");
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+        return totalPrice;
+      })
+    );
+    const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+    let newOrder = new Order({
+      orderItems: orderItemsIdsResolved,
+      shippingAddress: req.body.shippingAddress,
+      address: req.body.address,
+      status: req.body.status,
+      totalPrice: totalPrice,
+      customer: req.body.customer,
+      employee: req.locals.payload.employeeId,
+    });
+    newOrder
+      .save()
+      .then(() => {
+        Order.findById(newOrder._id)
+          .select({ __v: false, password: false })
+          .populate("customer", "name")
+          .populate({
+            path: "orderItems",
+            populate: {
+              path: "product",
+              populate: {
+                path: "category",
+                select: "name",
+              },
+              select: "_id name price",
+            },
+            select: "-__v",
+          })
+          .populate("employee", "name role")
+          .select("-__v")
+          .then((doc) => res.json(doc));
+      })
+      .catch((e) => {
+        res.status(400).json({
+          success: false,
+          error: e.message,
+          status: "The Order cannot be created",
+        });
+      });
+  });
 
 router.post("/totalsales", async function (req, res) {
-	let { startDate, endDate } = req.body
-	const totalSales = await Order.aggregate([
-		{
-			$match: {
-				dateOrdered: {
-					$gte: new Date(startDate),
-					$lte: new Date(endDate)
-				}
-			}
-		},
-		{
-			$group: {
-				_id: null,
-				totalsales: {
-					$sum: "$totalPrice"
-				},
-				totalOrders: {
-					$sum: 1
-				}
-			}
-		}
-	]);
-	result = totalSales.pop()
-	if (!result) {
-		return res.status(400).send("The order sales cannot be generated");
-	}
-	return res.json({
-		startDate: startDate,
-		endDate: endDate,
-		totalsales: result.totalsales,
-		totalOrders: result.totalOrders
-	});
+  let { startDate, endDate } = req.body;
+  const totalSales = await Order.aggregate([
+    {
+      $match: {
+        dateOrdered: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
+        },
+      },
+    },
+    {
+      $group: {
+        _id: null,
+        totalsales: {
+          $sum: "$totalPrice",
+        },
+        totalOrders: {
+          $sum: 1,
+        },
+      },
+    },
+  ]);
+  result = totalSales.pop();
+  if (!result) {
+    return res.status(400).send("The order sales cannot be generated");
+  }
+  return res.json({
+    startDate: startDate,
+    endDate: endDate,
+    totalsales: result.totalsales,
+    totalOrders: result.totalOrders,
+  });
 });
 router.get("/count", function (req, res) {
-	Order.countDocuments(function (err, count) {
-		if (!err) {
-			res.status(200).json({ orderCount: count });
-		} else {
-			res.send(err);
-		}
-	});
+  Order.countDocuments(function (err, count) {
+    if (!err) {
+      res.status(200).json({ orderCount: count });
+    } else {
+      res.send(err);
+    }
+  });
 });
 router.get("/customer/:customerId", async function (req, res) {
-	let page = req.query.page ? Number(req.query.page) : 1,
-		limit = req.query.limit ? Number(req.query.limit) : 20
-	page < 1 || isNaN(page) ? page = 1 : null
-	limit < 1 || isNaN(limit) ? limit = 20 : null
+  let page = req.query.page ? Number(req.query.page) : 1,
+    limit = req.query.limit ? Number(req.query.limit) : 20;
+  page < 1 || isNaN(page) ? (page = 1) : null;
+  limit < 1 || isNaN(limit) ? (limit = 20) : null;
 
-	Promise.all([
-		Order.find({customer: req.params.customerId})
-			.sort({ role: 1 })
-			.select({ __v: false, password: false })
-			.limit(limit)
-			.skip((page - 1) * limit)
-			.populate("customer", "name")
-			.populate({
-				path: "orderItems",
-				populate: {
-					path: "product",
-					populate: {
-						path: "category",
-						select: "name"
-					},
-					select: "_id name price"
-				},
-				select: "-__v"
-			})
-			.populate("employee", "name role")
-			.sort({ dateOrdered: -1 })
-			.select("-__v"),
-		Order.count({customer: req.params.customerId })
-	])
-		.then(([data, total]) => res.json({
-			currentPage: page,
-			limit: limit,
-			totalPage: total % limit != 0 ? parseInt(total / limit + 1) : total / limit,
-			data: data
-		}))
-		.catch(err => res.status(500).json({
-			isSuccess: false,
-			error: err,
-		}))
+  Promise.all([
+    Order.find({ customer: req.params.customerId })
+      .sort({ role: 1 })
+      .select({ __v: false, password: false })
+      .limit(limit)
+      .skip((page - 1) * limit)
+      .populate("customer", "name")
+      .populate({
+        path: "orderItems",
+        populate: {
+          path: "product",
+          populate: {
+            path: "category",
+            select: "name",
+          },
+          select: "_id name price",
+        },
+        select: "-__v",
+      })
+      .populate("employee", "name role")
+      .sort({ dateOrdered: -1 })
+      .select("-__v"),
+    Order.count({ customer: req.params.customerId }),
+  ])
+    .then(([data, total]) =>
+      res.json({
+        currentPage: page,
+        limit: limit,
+        totalPage:
+          total % limit != 0 ? parseInt(total / limit + 1) : total / limit,
+        data: data,
+      })
+    )
+    .catch((err) =>
+      res.status(500).json({
+        isSuccess: false,
+        error: err,
+      })
+    );
 });
 
-router.post("/toggle", (req,res)=>{
-	Order.findByIdAndUpdate(req.body._id, {status: req.body.status})
-			.then(()=>res.json({msg: "Update status success", status: req.body.status}))
-			.catch(err=>res.status(500).json({err: err.message}))
-})
+router.post("/toggle", (req, res) => {
+  Order.findByIdAndUpdate(req.body._id, { status: req.body.status })
+    .then(() =>
+      res.json({ msg: "Update status success", status: req.body.status })
+    )
+    .catch((err) => res.status(500).json({ err: err.message }));
+});
 
 router
-	.route("/:orderId")
-	.get(function (req, res) {
-		Order.findOne({ _id: req.params.orderId }).select("-__v")
-			.populate("customer", "name")
-			.populate({
-				path: "orderItems",
-				populate: {
-					path: "product",
-					populate: {
-						path: "category",
-						select: "name"
-					},
-					select: "_id name price"
-				},
-				select: "-__v"
-			})
-			.populate("employee", "name role")
-			.then(foundOrder => res.send(foundOrder))
-			.catch(err => res.status(404).json({
-				isSuccess: false,
-				message: "The order with the given ID was not found",
-			}))
-	})
-	.put(function (req, res) {
-		Order.findOneAndUpdate(
-			{ _id: req.params.orderId },
-			{
-				status: req.body.status,
-			},
-			{ new: true },
-			function (err) {
-				if (!err) {
-					res.status(200).send("Successfully updated status order.");
-				} else {
-					res.status(404).json({
-						success: false,
-						error: err.message,
-						message: "Invalid Order ID",
-						status: "The status order cannot updated",
-					});
-				}
-			}
-		);
-	})
-	.delete(function (req, res) {
-		Order.findByIdAndDelete(req.params.orderId)
-			.then(async (order) => {
-				if (order) {
-					await order.orderItems.map(async (orderItem) => {
-						await OrderItem.findByIdAndDelete(orderItem);
-					});
-					return res.status(200).json({
-						success: true,
-						message: "Successfully deleted the order",
-					});
-				} else {
-					return res.status(404).json({
-						sucess: false,
-						error: err.message,
-						message: "Invalid Order ID",
-						status: "The order cannot deleted.",
-					});
-				}
-			})
-			.catch((err) => {
-				return res.status(500).json({ success: false, error: err });
-			});
-	});
+  .route("/:orderId")
+  .get(function (req, res) {
+    Order.findOne({ _id: req.params.orderId })
+      .select("-__v")
+      .populate("customer", "name")
+      .populate({
+        path: "orderItems",
+        populate: {
+          path: "product",
+          populate: {
+            path: "category",
+            select: "name",
+          },
+          select: "_id name price",
+        },
+        select: "-__v",
+      })
+      .populate("employee", "name role")
+      .then((foundOrder) => res.send(foundOrder))
+      .catch((err) =>
+        res.status(404).json({
+          isSuccess: false,
+          message: "The order with the given ID was not found",
+        })
+      );
+  })
+  .put(function (req, res) {
+    Order.findOneAndUpdate(
+      { _id: req.params.orderId },
+      {
+        status: req.body.status,
+      },
+      { new: true },
+      function (err) {
+        if (!err) {
+          res.status(200).send("Successfully updated status order.");
+        } else {
+          res.status(404).json({
+            success: false,
+            error: err.message,
+            message: "Invalid Order ID",
+            status: "The status order cannot updated",
+          });
+        }
+      }
+    );
+  })
+  .delete(function (req, res) {
+    Order.findByIdAndDelete(req.params.orderId)
+      .then(async (order) => {
+        if (order) {
+          await order.orderItems.map(async (orderItem) => {
+            await OrderItem.findByIdAndDelete(orderItem);
+          });
+          return res.status(200).json({
+            success: true,
+            message: "Successfully deleted the order",
+          });
+        } else {
+          return res.status(404).json({
+            sucess: false,
+            error: err.message,
+            message: "Invalid Order ID",
+            status: "The order cannot deleted.",
+          });
+        }
+      })
+      .catch((err) => {
+        return res.status(500).json({ success: false, error: err });
+      });
+  });
+
+router
+  .route("/customer")
+  .get(async (req, res) => {
+    const orders = await Order.find({
+      customer: req.locals.payload.customerId,
+    });
+    return res.status(200).json(orders);
+  })
+  .post(async (req, res) => {
+    const orderItemsIds = Promise.all(
+      req.body.orderItems.map(async (orderitem) => {
+        const foundProduct = Product.findById(orderitem.product);
+        if (foundProduct) {
+          let newOrderItem = new OrderItem({
+            quantity: orderitem.quantity,
+            product: orderitem.product,
+          });
+          newOrderItem = await newOrderItem.save();
+          return newOrderItem._id;
+        }
+      })
+    ).catch((err) => {
+      return -1;
+    });
+    const orderItemsIdsResolved = await orderItemsIds;
+    if (orderItemsIdsResolved === -1) {
+      return res.status(404).json({
+        error: "Invalid ID Product",
+        message: "Cannot create orders",
+      });
+    }
+    const totalPrices = await Promise.all(
+      orderItemsIdsResolved.map(async (orderItemId) => {
+        const orderItem = await (
+          await OrderItem.findById(orderItemId)
+        ).populate("product", "price");
+        const totalPrice = orderItem.product.price * orderItem.quantity;
+        return totalPrice;
+      })
+    );
+    const totalPrice = totalPrices.reduce((a, b) => a + b, 0);
+    let newOrder = new Order({
+      orderItems: orderItemsIdsResolved,
+      shippingAddress: req.body.shippingAddress,
+      address: req.body.address,
+      status: req.body.status,
+      totalPrice: totalPrice,
+      customer: req.locals.payload.customerId,
+    });
+    newOrder
+      .save()
+      .then(() => {
+        Order.findById(newOrder._id)
+          .select({ __v: false, password: false })
+          .populate("customer", "name")
+          .populate({
+            path: "orderItems",
+            populate: {
+              path: "product",
+              populate: {
+                path: "category",
+                select: "name",
+              },
+              select: "_id name price",
+            },
+            select: "-__v",
+          })
+          .select("-__v")
+          .then((doc) => res.json(doc));
+      })
+      .catch((e) => {
+        res.status(400).json({
+          success: false,
+          error: e.message,
+          status: "The Order cannot be created",
+        });
+      });
+  });
 
 module.exports = router;
